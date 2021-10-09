784c45f64ee85ada68b1730239f69b9cd8c03b3a
Mehmet Ali <63293527+LykaMali@users.noreply.github.com>
2021-10-09 21:01:28 +0300

    Initial Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Ali on 9.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
